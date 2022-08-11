d4a4d4b40ef62562d6a30fa9bdf53c0da0345a35
riouha <4riouha@gmail.com>
2022-08-11 21:09:49 +0430

    add http proxy mw

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,9 @@ import { dbConnection } from "./db/db";
 import { errorHandlerMiddleware } from "../middlewares/error-handler.middleware";
 import { applicatonRouter } from "./app.router";
 import "./app.controller";
+import { createProxyMiddleware, fixRequestBody } from "http-proxy-middleware";
+import { passportService } from "../modules/user/services/passport.middleware";
+
 //===============================================================
 
 export default class Application {
@@ -26,6 +29,39 @@ export default class Application {
     this.app.use(express.urlencoded({ extended: true }));
 
     // # application routes
+    this.app.use(
+      "/test",
+      passportService.guard,
+      createProxyMiddleware({
+        target: "http://localhost:3000",
+        pathRewrite: { "^/test": "" },
+        changeOrigin: true,
+        onProxyReq: (proxyReq, req, res, opt) => {
+          console.log(`Route ` + req.headers.host + req.originalUrl + " to " + proxyReq.getHeaders().host + req.url);
+          console.log(req.body, req.params, req.query);
+          console.log(req.user);
+          if (req.body) fixRequestBody(proxyReq, req);
+        },
+        onProxyRes: (proxyRes, req, res) => {
+          console.info(
+            `get Response from ` +
+              proxyRes.socket.remoteAddress +
+              ":" +
+              proxyRes.socket.remotePort +
+              req.url +
+              " to " +
+              req.headers.host +
+              req.originalUrl
+          );
+          console.log(proxyRes.statusCode);
+          console.log(res.statusCode);
+        },
+        onError: (err: any, req: any, res: any) => {
+          console.error(err.message);
+        },
+      })
+    );
+
     this.app.use(
       "/",
       (req: Request, _res: Response, next: NextFunction) => {
